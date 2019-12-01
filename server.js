f86445d12235fc3269726e48141289a52cca874c
anderoonies <andrewbayer2016@u.northwestern.edu>
2019-11-30 20:24:30 -0500

    all the server stuff

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const socketIO = require('socket.io');
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const connectedSockets = {};
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, {root: __dirname}))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+const io = socketIO(server);
+io.on('connection', socket => {
+    socket.on('operations', operations => {
+        console.log(operations);
+        socket.broadcast.emit('operations', operations);
+    });
+    socket.on('siteId', siteId => {
+        socket.broadcast.emit('newSiteId', siteId);
+    })
+    socket.on('disconnect', () => console.log('Client disconnected'));
+});
